610659f05baf64202f9611e4c85747d2050a988b
Savannah Martin-Collins <savannahleemc@gmail.com>
2024-12-17 15:05:15 -0500

    add browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.scss'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-    </>
- 
+    <BrowserRouter>
+      <>
+      <Routes>
+          <Route path="/warehouses" element={<WarehousesPage />} />
+        </Routes>
+      </>
+    </BrowserRouter>
   )
 }
 
