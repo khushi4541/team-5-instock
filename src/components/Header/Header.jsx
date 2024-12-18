6d6a23d29ef8866992f54c1751fc3a692a85a872
SiviR-13 <sivi.rakaj@icloud.com>
2024-12-18 15:58:05 -0500

    header component and styling completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
+import Logo1 from "../../assets/logo/InStock-Logo_1x.png";
+import Logo2 from "../../assets/logo/InStock-Logo_2x.png";
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src={Logo1} alt="InStock Logo" className="header__logo-image1" />
+        <img src={Logo2} alt="InStock Logo" className="header__logo-image2" />
+      </div>
+      <div className="header__nav">
+       
+            <Link to="/warehouses" className="header__nav-link1">
+              Warehouses
+            </Link>
+         
+            <Link to="/inventory" className="header__nav-link2">
+              Inventory
+            </Link>
+          
+     
+      </div>
+    </header>
+  );
+}
