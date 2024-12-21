5c4cf069f1d330abc346fb49740029e0ab052f99
SiviR-13 <sivi.rakaj@icloud.com>
2024-12-20 19:04:01 -0500

    changes made

--- src/components/InventoryItemDetails/InventoryItemDetails.jsx
+++ src/components/InventoryItemDetails/InventoryItemDetails.jsx
@@ -2,14 +2,16 @@ import "./InventoryItemDetails.scss";
 import { Link } from "react-router-dom";
 
 function InventoryItemDetails({ getInventoryById }) {
+  const statusClass =
+    getInventoryById.status.toLowerCase() === "in stock"
+      ? "items__status-tag--in-stock"
+      : "items__status-tag--out-of-stock";
+
   return (
     <article className="item">
-      <div className="item__details">
-        <div className="item__location">
-          <Link
-            to={`/inventories/${getInventoryById.id}`}
-            className="item__inventory"
-          >
+      <div className="item__header">
+        <div className="item__selected">
+          <Link to="/inventory" className="item__back-link">
             <svg
               className="item__arrow"
               width="24"
@@ -23,8 +25,33 @@ function InventoryItemDetails({ getInventoryById }) {
                 fill="#2E66E6"
               />
             </svg>
-            <p className="item__name">{getInventoryById.item_name}</p>
-            
           </Link>
+          <p className="item__name">{getInventoryById.item_name}</p>
+        </div>
+        <button className="item__edit-button">Edit</button>
+      </div>
 
-         
+      <div className="item__details">
+        <div className="item__info-group">
+          <h4 className="item__label">Item Description</h4>
+          <p className="item__info">{getInventoryById.description}</p>
+          <h4 className="item__label">Category</h4>
+          <p className="item__info">{getInventoryById.category}</p>
+        </div>
+
+        <div className="item__info-group">
+          <h4 className="item__label">Status</h4>
+          <p className={`item__status-tag ${statusClass}`}>
+            {getInventoryById.status}
+          </p>
+          <h4 className="item__label">QTY</h4>
+          <p className="item__info">{getInventoryById.quantity}</p>
+          <h4 className="item__label">Warehouse</h4>
+          <p className="item__info">{getInventoryById.warehouse_name}</p>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default InventoryItemDetails;
--- src/pages/InventoryDetailsPage/InventoryDetailsPage.jsx
+++ src/pages/InventoryDetailsPage/InventoryDetailsPage.jsx
@@ -1,7 +1,19 @@
+import InventorysList from "../../components/InventoryList/InventoryList";
+
 export default function InventoryDetailsPage() {
     return (
         <>
         <h1>Product Name</h1>
         </>
     )
+}
+
+import InventorysList from "../../components/InventoryList/InventoryList";
+
+export default function InventoryPage() {
+  return (
+    <>
+      <InventorysList />
+    </>
+  );
 }
