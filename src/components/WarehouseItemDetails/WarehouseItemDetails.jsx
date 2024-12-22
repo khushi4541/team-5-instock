bc5940211cbe221f6bbe6d77e9b34a4697ce7019
SiviR-13 <sivi.rakaj@icloud.com>
2024-12-21 21:16:08 -0500

    styling and functionality completed-missing onky the divider

--- src/components/WarehouseItemDetails/WarehouseItemDetails.jsx
+++ src/components/WarehouseItemDetails/WarehouseItemDetails.jsx
@@ -1,158 +1,95 @@
 import "./WarehouseItemDetails.scss";
-import { useLocation, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { baseURL } from "../../utils/api";
 
-// import WarehouseInventory from "../WarehouseInventory/WarehouseInventory";
+export default function WarehouseCardDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [warehouse, setWarehouse] = useState(null);
 
-export default function WarehouseDetails() {
-  const location = useLocation();
-  const warehouse = location.state?.warehouse;
+  useEffect(() => {
+    const fetchWarehouse = async () => {
+      const url = `${baseURL}/warehouses/${id}`;
+      try {
+        const response = await axios.get(url);
+        setWarehouse(response.data);
+      } catch (error) {
+        console.error("Error fetching warehouse details:", error);
+      }
+    };
+    fetchWarehouse();
+  }, [id]);
 
   if (!warehouse) {
-    return (
-      <div className="warehouse-error-message">
-        No warehouse data available.
-      </div>
-    );
+    return <p>Loading warehouse details...</p>;
   }
 
   return (
-    <>
-      <div className="warehouse-details-container">
-        <div className="warehouse-details">
-          <div className="warehouse-header">
-            <Link to="/" className="warehouse-header-back">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
-                  fill="#2E66E6"
-                />
-              </svg>
-            </Link>
-            <h1 className="warehouse-header-title">
-              {warehouse.warehouse_name}
-            </h1>
-            <a className="warehouse-header-edit">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z"
-                  fill="#FFFFFF"
-                />
-              </svg>
-              <span className="warehouse-edit-text">Edit</span>
-            </a>
+    <article className="card">
+      <div className="card__header">
+        <div className="card__selected">
+          <div
+            className="card__back-link"
+            onClick={() => navigate(-1)}
+            style={{ cursor: "pointer" }}
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
+                fill="#2E66E6"
+              />
+            </svg>
           </div>
-          <ul className="warehouse-info-list">
-            <div className="warehouse-text-wrapper">
-              <div className="warehouse-info-wrapper">
-                <li className="warehouse-info-item warehouse-address">
-                  <h4 className="warehouse-section-title">Warehouse Address</h4>
-                  <p className="warehouse-address-details">
-                    {warehouse.address}, {warehouse.city}, {warehouse.country}
-                  </p>
-                </li>
-                <li className="warehouse-info-item warehouse-contact">
-                  <div className="warehouse-contact-details">
-                    <h4 className="warehouse-section-title">Contact Name</h4>
-                    <p className="contact-person-name">
-                      {warehouse.contact_name}
-                    </p>
-                    <p className="contact-person-role">
-                      {warehouse.contact_position}
-                    </p>
-                  </div>
-                  <div className="warehouse-contact-info">
-                    <h4 className="warehouse-section-title">
-                      Contact Information
-                    </h4>
-                    <p className="contact-phone">{warehouse.contact_phone}</p>
-                    <p className="contact-email">{warehouse.contact_email}</p>
-                  </div>
-                </li>
-              </div>
-            </div>
-          </ul>
+          <h1 className="card__name">{warehouse.warehouse_name}</h1>
         </div>
-        <div className="inventory-header">
-          <ul className="inventory-columns">
-            <li className="inventory-column-title">
-              <h4 className="inventory-label">Inventory Item</h4>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
-                  fill="#5C667E"
-                />
-              </svg>
-            </li>
-            <li className="inventory-column-title">
-              <h4 className="inventory-label">Category</h4>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
-                  fill="#5C667E"
-                />
-              </svg>
-            </li>
-            <li className="inventory-column-title">
-              <h4 className="inventory-label">Status</h4>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
-                  fill="#5C667E"
-                />
-              </svg>
-            </li>
-            <li className="inventory-column-title">
-              <h4 className="inventory-label">Quantity</h4>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
-                  fill="#5C667E"
-                />
-              </svg>
-            </li>
-            <li className="inventory-column-title inventory-actions">
-              <h4 className="inventory-label">Actions</h4>
-            </li>
-          </ul>
-        </div>
-        {/* <WarehouseInventory warehouseId={warehouse.id} warehouse={warehouse} /> */}
+        <button
+          className="card__headeredit"
+          onClick={() => navigate(`/warehouses/${id}/edit`)}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z"
+              fill="#FFFFFF"
+            />
+          </svg>
+          Edit
+        </button>
       </div>
-    </>
+      <div className="card__contact">
+        <div className="card__adress">
+          <h4 className="card_lab">ADDRESS</h4>
+          <p className="card__info">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</p>
+        </div>
+
+        <div className="card__other">
+          <div className="card__item">
+            <h4 className="card__lab">CONTACT NAME</h4>
+            <p className="card__info">{warehouse.contact_name}</p>
+            
+          </div>
+          <div className="card__contact-info">
+          <h4 className="card__lab">CONTACT INFORMATION</h4>
+            <p className="card__info item__info--phone">
+              {warehouse.contact_phone}
+            </p>
+            <p className="card__info">{warehouse.contact_email}</p>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 }
