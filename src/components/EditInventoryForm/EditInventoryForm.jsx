bf4742c123ddf9c4d810046141a9d66097dea637
Khushi Parekh <khushi4541@gmail.com>
2024-12-22 21:22:28 -0500

    added all elements into the jsx

--- src/components/EditInventoryForm/EditInventoryForm.jsx
+++ src/components/EditInventoryForm/EditInventoryForm.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./EditInventoryForm.scss";
 
 function EditInventoryForm({ fetchWarehouses, warehousesData }) {
@@ -15,8 +15,31 @@ function EditInventoryForm({ fetchWarehouses, warehousesData }) {
 
   const navigate = useNavigate();
   const { inventoryId } = useParams();
+  const categories = [
+    "Electronics",
+    "Gear",
+    "Apparel",
+    "Accessories",
+    "Health",
+  ];
 
-  
+  useEffect(() => {
+    fetchWarehouses();
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleStatusChange = (e) => {
+    const value = e.target.value;
+    setFormData((prevData) => ({
+      ...prevData,
+      status: value,
+      quantity: value === "Out of Stock" ? "" : prevData.quantity, // Clear quantity if status is "Out of Stock"
+    }));
+  };
 
   const handleBack = () => {
     navigate(-1);
@@ -46,6 +69,7 @@ function EditInventoryForm({ fetchWarehouses, warehousesData }) {
               type="text"
               className="edit-inventory__input"
               id="item-name"
+              onChange={handleInputChange}
             />
           </div>
           <div className="edit-inventory__form-group">
@@ -56,17 +80,84 @@ function EditInventoryForm({ fetchWarehouses, warehousesData }) {
               type="text"
               className="edit-inventory__textarea"
               id="item-description"
+              onChange={handleInputChange}
             />
           </div>
+          <div className="edit-inventory__form-group">
+            <h3 className="edit-inventory__label">Category</h3>
+            <select
+              className="edit-inventory__dropdown"
+              name="warehouse_id"
+              value={formData.warehouse_id}
+              onChange={handleInputChange}
+            >
+              <option value="">Select a Category</option>
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+            {errors.warehouse_id && <p>{errors.warehouse_id}</p>}
+          </div>
+        </article>
+
+        <article className="edit-inventory__section">
+          <h2 className="edit-inventory__title">Warehouse Details</h2>
           <div className="edit-inventory__form-group">
             <h3 htmlFor="item-category" className="edit-inventory__label">
               Item Name
             </h3>
-            <input
-              type="text"
-              className="edit-inventory__input"
-              id="item-name"
-            />
+            <label className="edit-inventory__radio-label">
+              <input
+                type="radio"
+                name="status"
+                value="In Stock"
+                checked={formData.status === "In Stock"}
+                onChange={handleStatusChange}
+              />
+              In Stock
+            </label>
+            <label className="edit-inventory__radio-label">
+              <input
+                type="radio"
+                name="status"
+                value="Out of Stock"
+                checked={formData.status === "Out of Stock"}
+                onChange={handleStatusChange}
+              />
+              Out of Stock
+            </label>
+          </div>
+          {formData.status === "In Stock" && (
+            <div>
+              <h3 className="edit-inventory__label">Quantity</h3>
+              <input
+                className="edit-inventory__input"
+                type="number"
+                name="quantity"
+                value={formData.quantity}
+                onChange={handleInputChange}
+              />
+              {errors.quantity && <p>{errors.quantity}</p>}
+            </div>
+          )}
+          <div className="edit-inventory__form-group">
+            <h3 className="edit-inventory__label"> Warehouse</h3>
+            <select
+              className="edit-inventory__dropdown"
+              name="warehouse_id"
+              value={formData.warehouse_id}
+              onChange={handleInputChange}
+            >
+              <option value="">Select a warehouse</option>
+              {warehousesData.map((warehouse) => (
+                <option key={warehouse.id} value={warehouse.id}>
+                  {warehouse.warehouse_name}
+                </option>
+              ))}
+            </select>
+            {errors.warehouse_id && <p>{errors.warehouse_id}</p>}
           </div>
         </article>
       </div>
